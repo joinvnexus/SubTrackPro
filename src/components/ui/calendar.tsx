e58cfcc427db35d56ba0120244d81ccc5c65894f
Joinvnexus <projoynaidu407@gmail.com>
2026-02-27 19:29:57 +0600

    fix react 19 peer deps and update daypicker v9 calendar icons

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,8 +52,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: ({ className, orientation, ...props }) =>
+          orientation === "left" ? (
+            <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+          ) : (
+            <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+          ),
       }}
       {...props}
     />
